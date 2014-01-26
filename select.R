c640bdf22b0321b99c53095030c06a3ce7658dd3
maxconway <conway.max1@gmail.com>
2014-01-26 19:59:18 +0000

    Added comment to select

--- select.R
+++ select.R
@@ -1,4 +1,12 @@
 select <- function(population, maxpop){
+	# Sorts and cuts population
+	#
+	# Args:
+	#  population: the whole population datasctructure
+	#  maxpop: int, the maximum total population
+	#
+	# Returns:
+	#  A subset of the elements of population, ordered by rank and crowding distance
 	head(population[order(
 		sapply(population, function(x){x$rank}),
 		-sapply(population, function(x){x$crowding}),
