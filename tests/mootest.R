b244474d550a76c3f73c7927c44e30ce14b999c1
maxconway <conway.max1@gmail.com>
2014-01-26 21:07:47 +0000

    test of mature
    - also correction to testpop

--- tests/mootest.R
+++ tests/mootest.R
@@ -11,14 +11,25 @@ evaluate <- function(x){
 # starting point:
 x <- list(genotype = c(0,0,0,0,0,0))
 # sample population
-testpop <- list(list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
-								list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
-								list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9))
-								)
+testpop <- list(list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
+								list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
+								list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9))
+)
 
 test_that('reproduce gives a plausible result', {
 	pop <- 10
 	res <- reproduce(testpop, pop)
 	
-	expect_equal(length(res), pop)
+	expect_identical(length(res), pop)
+})
+
+test_that('mature gives a plausible result', {
+	res <- mature(testpop, evaluate)
+	
+	expectedres <- list(list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 3, proptwo = 3)),
+											list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 3, proptwo = 3)),
+											list(genotype = c(1,1,1,1,1,1), phenotype = c(propone = 3, proptwo = 3))
+	)
+	
+	expect_identical(res, expectedres)
 })
