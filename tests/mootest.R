f9818fedfe10c388d4386d87103e59e3f7234325
maxconway <conway.max1@gmail.com>
2014-01-26 20:59:26 +0000

    Testing infrastructure

--- tests/mootest.R
+++ tests/mootest.R
@@ -4,11 +4,16 @@ context('MMO code')
 
 # evaluation function:
 evaluate <- function(x){
-	c(propertyone = abs(x-c(1,1,1,0,0,0)), 
-		propertyone = abs(x-c(0,0,0,1,1,1))
+	c(propone = sum(abs(x-c(1,1,1,0,0,0))), 
+		proptwo = sum(abs(x-c(0,0,0,1,1,1)))
 	)
 }
 # starting point:
 x <- list(genotype = c(0,0,0,0,0,0))
+# sample population
+testpop <- list(list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
+								list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9)),
+								list(genotype = c(1,1,1,1,1,1,1), phenotype = c(propone = 9, proptwo = 9))
+								)
 
 ## run a test using it
