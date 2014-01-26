be9e7282916133e6740547591e6a80ecfb9cb753
maxconway <conway.max1@gmail.com>
2014-01-26 17:33:29 +0000

    Carried through evaluate function
    also some formatting changes

--- GDMO.R
+++ GDMO.R
@@ -1,8 +1,8 @@
-GDMO <- function(pop,gen,startingpoint){
+GDMO <- function(pop, gen, startingpoint, evaluate){
 	parents <- startingpoint
 	while(currentgen <= gen){
 		children <- reproduce(parents = parents)
-		population <- nondomsort(list(mature(children),parents))
+		population <- nondomsort(list(mature(children, evaluate), parents))
 		parents <- select(population, maxpop = pop)
 	}
 }
--- select.R
+++ select.R
@@ -1,7 +1,7 @@
 select <- function(population, maxpop){
 	head(population[order(
-		sapply(population,function(x){x$rank}),
-		-sapply(population,function(x){x$crowding}),
+		sapply(population, function(x){x$rank}),
+		-sapply(population, function(x){x$crowding}),
 		runif(length(population))
 		)],
 			 n = maxpop)
