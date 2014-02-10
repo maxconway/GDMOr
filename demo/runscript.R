1cec57ec34b62a185c625230018f82eae022dcc5
maxconway <conway.max1@gmail.com>
2014-02-10 13:17:41 +0000

    uncommented code, since runscript is now a demo

--- demo/runscript.R
+++ demo/runscript.R
@@ -5,26 +5,25 @@
 library(sybil)
 library(sybilSBML)
 library(glpkAPI)
-# TODO: Load optimization software
 
 # Load data
-# mp <- system.file(package = "sybil", "extdata")
-# Ec_core <- readTSVmod(prefix = "Ec_core", fpath = mp, quoteChar = "\"")
+mp <- system.file(package = "sybil", "extdata")
+Ec_core <- readTSVmod(prefix = "Ec_core", fpath = mp, quoteChar = "\"")
 
 # Create starting point
-# start <- as.list(rep(TRUE, times = length(allGenes(Ec_core))))
-# names(start) <- allGenes(Ec_core)
-# ancestors <- list(list(genotype = start))
+start <- as.list(rep(TRUE, times = length(allGenes(Ec_core))))
+names(start) <- allGenes(Ec_core)
+ancestors <- list(list(genotype = start))
 
 # create evaluation function
-# evaluate <- function(genotype){
-# 	solution <- optimizeProb(object = Ec_core, 
-# 													 gene = names(genotype)[genotype==FALSE],
-# 													 lb = 0,
-# 													 ub = 0,
-# 													 retOptSol = FALSE)
-# 	solution$fluxes[match(c('Biomass_Ecoli_core_w_GAM', 'EX_ac(e)'),react_id(Ec_core))]
-# }
+evaluate <- function(genotype){
+	solution <- optimizeProb(object = Ec_core, 
+													 gene = names(genotype)[genotype==FALSE],
+													 lb = 0,
+													 ub = 0,
+													 retOptSol = FALSE)
+	solution$fluxes[match(c('Biomass_Ecoli_core_w_GAM', 'EX_ac(e)'),react_id(Ec_core))]
+}
 
-# GDMO(100, 100, ancestors, evaluate)
+GDMO(100, 100, ancestors, evaluate)
 
