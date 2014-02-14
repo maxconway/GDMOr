2c7eeda1c2ce6687a6a9502992da908d2e23903a
maxconway <conway.max1@gmail.com>
2014-02-14 14:57:09 +0000

    checkpointer

--- R/GDMO.R
+++ R/GDMO.R
@@ -14,12 +14,16 @@ GDMO <- function(population, generations, startingpoint, evaluate){
 	currentgen <- 1
 	
 	genbar <- txtProgressBar(min = 0, max = generations, style=3)
+	starttime <- proc.time()[3]
 	tryCatch(
 		while(currentgen <= generations){
 			children <- reproduce(parents = parents, pop = population)
 			adults <- nondomsort(c(mature(children, evaluate), parents))
 			parents <- select(adults, maxpop = population)
 			currentgen <- currentgen + 1
+			if(proc.time()[3] - starttime > 300 && currentgen%%10==0){
+				save(parents, file='checkpoint.RData')
+			}
 			setTxtProgressBar(genbar, currentgen)
 		},
 		finally = return(parents)
