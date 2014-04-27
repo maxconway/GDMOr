cf3f281fb37f6b068d1f0d0662a9f970ba857e9d
maxconway <conway.max1@gmail.com>
2014-04-27 20:12:29 +0100

    Actually solved layout issues after all

--- R/outlyingGenes.R
+++ R/outlyingGenes.R
@@ -59,33 +59,39 @@ outlyingGenes <- function(dataset, lowerlimit=-2, upperlimit=2, genes_subsystems
 	
 	distributionPlot <- bothplots(ggplot(a[abs(a$correlation)!=0,])
 																+geom_point(aes(x=correlation,y=name, colour = subSystem))
-																+ try_default({
-																	geom_text(data = a[a$correlation>(upperlimit*sd(a.norm$correlation,na.rm=T)) | a$correlation<(lowerlimit*sd(a.norm$correlation,na.rm=T)),],
-																						aes(x = correlation,
-																								size = 2.5,
-																								y = name,
-																								label = round(expression,3),
-																								hjust = 0.5+0.5*-sign(correlation)
-																						),
-																						show_guide = FALSE
-																	)
-																},
-																NULL
-																)
 	)
 	
-	subsystemPlot <- a %.% 
-		group_by(subSystem) %.% 
-		summarise(correlation = mean(correlation)) %.% 
-		ggplot(aes(x=subSystem, y=correlation, fill = subSystem)) +
-		geom_bar(stat='identity') + scale_fill_discrete() +
-		coord_flip() + theme(legend.position="none", axis.text.y=element_text(angle=45, colour='gray30'))
+	if(1<=nrow(a[a$correlation>(upperlimit*sd(a.norm$correlation,na.rm=T)) | a$correlation<(lowerlimit*sd(a.norm$correlation,na.rm=T)),])){
+		distributionplot <- distributionplot + geom_text(data = a[a$correlation>(upperlimit*sd(a.norm$correlation,na.rm=T)) | a$correlation<(lowerlimit*sd(a.norm$correlation,na.rm=T)),],
+																										 aes(x = correlation,
+																										 		size = 2.5,
+																										 		y = name,
+																										 		label = round(expression,3),
+																										 		hjust = 0.5+0.5*-sign(correlation)
+																										 ),
+																										 show_guide = FALSE
+		)
+	}
+	
+	if(!missing(genes_subsystems)){
+		subsystemPlot <- a %.% 
+			group_by(subSystem) %.% 
+			summarise(correlation = mean(correlation)) %.% 
+			ggplot(aes(x=subSystem, y=correlation, fill = subSystem)) +
+			geom_bar(stat='identity') + scale_fill_discrete() +
+			coord_flip() + theme(legend.position="none", axis.text.y=element_text(angle=45, colour='gray30'))
+	}
 	
 	grid.newpage()
 	pushViewport(viewport(layout=grid.layout(5,5)))
-	print(densityPlot,vp=viewport(layout.pos.row=1,layout.pos.col=3:5))
-	print(distributionPlot,vp=viewport(layout.pos.row=2:5,layout.pos.col=3:5))
-	print(subsystemPlot,vp=viewport(layout.pos.row=1:5, layout.pos.col=1:2))
+	if(!missing(genes_subsystems)){
+		print(subsystemPlot,vp=viewport(layout.pos.row=1:5, layout.pos.col=1:2))
+		verticaldivide <- 3
+	}else{
+		verticaldivide <- 1
+	}
+	print(densityPlot,vp=viewport(layout.pos.row=1,layout.pos.col=verticaldivide:5))
+	print(distributionPlot,vp=viewport(layout.pos.row=2:5,layout.pos.col=verticaldivide:5))
 	
 	return(na.omit(a[a$correlation>(upperlimit*sd(a.norm$correlation,na.rm=T)) | a$correlation<(lowerlimit*sd(a.norm$correlation,na.rm=T)),]))
 }
