7dcb052e71d32fd7aaa9bb1c03b541ba01ac07d0
maxconway <conway.max1@gmail.com>
2014-04-27 19:52:33 +0100

    Added a wrapper for performing GDMO on sybil models
    - memoized it
    - maybe should be using generics

--- R/GDMO.R
+++ R/GDMO.R
@@ -31,3 +31,49 @@ GDMO <- function(population, generations, startingpoint, evaluate, savefile = 'c
 #		finally = return(parents)
 #	)
 }
+
+GDMO_sybil <- function(model, population=10, generations=10, targets, targetdirections, savefile = 'checkpoint.RData'){
+	
+	# Create starting point
+	start_genotype <- as.list(rep(TRUE, times = length(allGenes(model))))
+	start_phenotype <- as.list(rep(NULL, times = length(targets)+1))
+	names(start_genotype) <- allGenes(model)
+	names(start_phenotype) <- c(targets,'kos') 
+	ancestors <- list(list(genotype = start_genotype, phenotype = start_phenotype))
+	
+	# set up memoization
+	memo <- list()
+	
+	# create evaluation function
+	evaluate <- function(genotype){
+		
+		# Check memoization
+		phenotype <- memo[[digest(genotype)]]
+		if(!is.null(phenotype)){
+			return(phenotype)
+		}
+		
+		
+		# avoid an error with no knockouts
+		# sybil should really deal with this
+		genes <- if(suppressWarnings(all(genotype))){
+			NULL
+		} else{
+			names(genotype)[genotype==FALSE]
+		}
+		# call sybil to compute the fluxes
+		solution <- optimizeProb(object = Ec_core, 
+														 gene = genes,
+														 lb = 0,
+														 ub = 0,
+														 retOptSol = FALSE)
+		phenotype <- c(solution$fluxes[match(targets,react_id(Ec_core))]*-targetdirections, sum(genotype==FALSE))
+		
+		# memoize
+		memo[[digest(genotype)]] <<- phenotype
+		return(phenotype)
+	}
+	
+	return(GDMO(population, generations, ancestors, evaluate, savefile))
+	
+}
