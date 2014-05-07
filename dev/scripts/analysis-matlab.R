77d86a7c62f9bdd98ac894c8459b50c80cc29850
maxconway <conway.max1@gmail.com>
2014-05-07 14:08:19 +0100

    correct phenotype names

--- dev/scripts/analysis-matlab.R
+++ dev/scripts/analysis-matlab.R
@@ -13,7 +13,7 @@ load_all()
 
 chrom <- load_pop_matlab(files=list.files(path='dev/Matlab/expFBA/solutions',pattern='solution1\\d\\d0.mat',full.names=TRUE),
 												 genotype_names=unlist(readMat('dev/Matlab/expFBA/geni.mat')$geni),
-												 phenotype_names=c('obj1','obj2'))
+												 phenotype_names=c('biomass','atp'))
 chrom <- chrom[sample.int(n=nrow(chrom),size=500),]
 
 load(file='./dev/raw/PNASMS2013-07797_DatasetS2_mutli_ecoli_55_models.RData')
@@ -21,7 +21,7 @@ load(file='./dev/raw/PNASMS2013-07797_DatasetS2_mutli_ecoli_55_models.RData')
 model <- models$iJO1366
 cw <- cytoscape_load(model=models$iJO1366)
 
-point_selector(dataset=chrom, x='phenotype.obj1', y='phenotype.obj2', model=model, cw=cw)
+point_selector(dataset=chrom, x='phenotype.biomass', y='phenotype.atp', model=model, cw=cw)
 
 
 
