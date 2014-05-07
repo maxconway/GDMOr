965404f1d899e7d5f6465e0a94b12842ad2f60dd
maxconway <conway.max1@gmail.com>
2014-05-07 14:03:47 +0100

    Formalized CAI code into function with tests

--- dev/scripts/myCAI.R
+++ dev/scripts/myCAI.R
@@ -1,6 +1,7 @@
 # My take on CAI
 
 library(stringr)
+library(genomes)
 
 
 # Using functions pulled from CAI_alaSharp, we pull two tables.
@@ -9,76 +10,4 @@ ftpURL <- urlParserftoTONCBI(ftp$ftp[7])
 anngenes <- getAnnotation(ftpURL)
 DNA <- getDNA(ftpURL)
 
-#' Calculate the CAI for each gene
-#' 
-#' @param DNA a DNAString object, giving the whole genome
-#' @param anngenes a GRanges object, detailing how genes match to the genome
-#' @param referenceGenes the genes selected as being highly expressed enough to form a reference set
-getCAIs <- function (DNA, anngenes, referenceGenes = anngenes@elementMetadata@listData$locus) {
-	
-	# make a list of amino acids, and their codons
-	codontable <- list(Ala = c('GCT', 'GCC', 'GCA', 'GCG'),
-										 Arg = c('CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
-										 Asn = c('AAT', 'AAC'),
-										 Asp = c('GAT', 'GAC'),
-										 Cys = c('TGT', 'TGC'),
-										 Gln = c('CAA', 'CAG'),
-										 Glu = c('GAA', 'GAG'),
-										 Gly = c('GGT', 'GGC', 'GGA', 'GGG'),
-										 His = c('CAT', 'CAC'),
-										 Ile = c('ATT', 'ATC', 'ATA'),
-										 Leu = c('TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'),
-										 Lys = c('AAA', 'AAG'),
-										 Met = c('ATG'),
-										 Phe = c('TTT', 'TTC'),
-										 Pro = c('CCT', 'CCC', 'CCA', 'CCG'),
-										 Ser = c('TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'),
-										 Thr = c('ACT', 'ACC', 'ACA', 'ACG'),
-										 Trp = c('TGG'),
-										 Tyr = c('TAT', 'TAC'),
-										 Val = c('GTT', 'GTC', 'GTA', 'GTG'),
-										 # 									 START = c('ATG'),
-										 STOP = c('TAA', 'TGA', 'TAG')
-	)
-	# And prepare a vector of codons for making factors
-	codons <- unlist(codontable, use.names=FALSE)
-	
-	
-  # anngenes contains a dataframe of genes with their positions.
-  # DNA is the DNA sequence.
-  
-  # using these, we cna find the sequence for each gene:
-  genesView <- Views(DNA, anngenes@ranges)
-  
-  # correct the senses and convert to characters
-  genes <- ifelse(as.character(anngenes@strand)=='+',
-  								as.character(genesView),
-  								as.character(reverseComplement(genesView))
-  								)
-	
-	# add names
-	names(genes) <- anngenes@elementMetadata@listData$locus
-  
-  # now factorize into codons
-  genes <- lapply(genes, function(code){
-  	factor(substring(code,seq.int(1,str_length(code),3),seq.int(3,str_length(code),3)),codons)
-  })
-  
-  # Find out total number of usages
-  globaltable <- table(unlist(genes[anngenes@elementMetadata@listData$locus %in% referenceGenes]))
-  codonweights <- lapply(codontable, function(codons){
-  	aminoacid <- globaltable[codons]
-  	aminoacid/max(aminoacid)
-  })
-  # And clean up to a nice vector
-  names(codonweights) <- NULL
-  codonweights <- unlist(codonweights)
-	
-	# now it's time for the actual calculation
-	CAIs <- sapply(genes, function(codons){
-		exp(mean(log(codonweights[codons]),na.rm=TRUE))
-	})
-	
-}
-
-
+CAIs <- getCAIs(DNA,anngenes)
---
+++ inst/tests/test-getCAIs.R
@@ -0,0 +1,38 @@
+context('getCAIs')
+
+# Set up defaults
+names <- c('one','two','three')
+DNA <- DNAString(paste(rep('A',27),collapse=''))
+ranges <- IRanges(start=c(1,10,19),
+									width=9,
+									names=names
+)
+strand = Rle('+',length(names))
+granges <- GRanges(seqnames = c('test1'),
+									 ranges = ranges,
+									 strand = strand
+)
+granges@elementMetadata$locus <- names
+
+test_that('runs', {
+	sample <- getCAIs(DNA,granges)
+	hypothesis <- c(one=1,two=1,three=1)
+	expect_that(sample, equals(hypothesis))
+})
+
+test_that('realistic case one', {
+	DNA <- DNAString('GATGATGATGATGATGATGACGACGAC')
+	
+	sample <- getCAIs(DNA,granges)
+	hypothesis <- c(one=1,two=1,three=1/2)
+	expect_that(sample, equals(hypothesis))
+})
+
+test_that('strands work', {
+	strand(granges) <- c('+','-','+')
+	DNAString <- DNAString('AAAAAAAAATTTTTTTTTAAAAAAAAA')
+	
+	sample <- getCAIs(DNA,granges)
+	hypothesis <- c(one=1,two=1,three=1)
+	expect_that(sample, equals(hypothesis))
+})
