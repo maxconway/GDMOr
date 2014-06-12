e1a54def6260eefb1f5028f931829a0005df5176
maxconway <conway.max1@gmail.com>
2014-06-12 18:02:26 +0100

    Complete rewrite
    - moved to S3
    - performance improvement, largely due to only parsing once where possible

--- R/gene2rxn.R
+++ R/gene2rxn.R
@@ -1,10 +1,11 @@
 #' @title gene to reaction lookup
 #'   
-#' @description Takes a sybil models and a character vector of active genes, and
+#' @description Takes a sybil model and a data frame or vector of gene activity, and
 #' finds active reactions from this.
 #' 
+#' @param genes a data frame or vector of gene activity
 #' @param model a sybil model
-#' @param genes a vector vector of gene presence or expression
+#' @param env an environment in which to evaluate the gene-reaction mappings
 #' 
 #' 
 #' @return a vector of reaction presence of activity
@@ -16,7 +17,43 @@
 #' @import sybil
 #' 
 #' @export
-gene2rxn <- function(model, genes){
+#' @rdname gene2rxn
+gene2rxn <- function(genes, model, env=new.env(emptyenv())){
+	UseMethod('gene2rxn')
+}
+
+#' @export
+#' @rdname gene2rxn
+gene2rxn.data.frame <- function(genes, model, env=new.env(emptyenv())){
+	exprlist <- llply(model@gprRules, function(rule){parse(text=rule)})
+	exprlist[model@gprRules==''] <- expression(1)
+	
+	if(any(sapply(genes,is.numeric))){
+		assign('&', function(x,y){min(x,y)}, env)
+		assign('|', function(x,y){max(x,y)}, env)
+		toeval <- function(g){gene2rxn.default(model=model, genes=genes[g,], env=env, exprlist=exprlist)}
+	}else{
+		toeval <- function(g){gene2rxn.default(model=model, genes=genes[g,], env=env, exprlist=exprlist)}
+	}
+	toeval <- cmpfun(toeval, options=list(optimize=3))
+	
+	eval(compile(options=list(optimize=3),
+							 ldply(1:nrow(genes), toeval)
+	))
+}
+
+#' @export
+#' @rdname gene2rxn
+gene2rxn.numeric <- function(genes, model, env=new.env(emptyenv()), exprlist=NULL){
+	assign('&', function(x,y){min(x,y)}, env)
+	assign('|', function(x,y){max(x,y)}, env)
+	
+	gene2rxn.default(genes, model, env, exprlist)
+}
+
+#' @export
+#' @rdname gene2rxn
+gene2rxn.default <- function(genes, model, env=new.env(emptyenv()), exprlist=NULL){
 	# check arguments
 	if(length(genes) != length(model@allGenes)){
 		stop('requires a vector of gene presence. Try model@allGenes %in% genes')
@@ -25,14 +62,15 @@ gene2rxn <- function(model, genes){
 	env <- new.env(parent = baseenv())
 	assign('x', genes, env)
 	
-	if(is.numeric(genes)){
-		assign('&', function(x,y){min(x,y)}, env)
-		assign('|', function(x,y){max(x,y)}, env)
+	if(is.null(exprlist)){
+		exprlist <- llply(model@gprRules, function(rule){parse(text=rule)})
+		exprlist[model@gprRules==''] <- expression(1)
 	}
 	
-	rxnsactivity <- laply(model@gprRules,function(rule){
-		res <- eval(expr=parse(text=rule),envir=env)
-		ifelse(is.null(res),1,as.numeric(res))
-	})
+	rxnsactivity <- vapply(exprlist, function(expr){eval(expr=expr, envir=env)[[1]]},1)
+	
+	stopifnot(length(rxnsactivity)==model@react_num)
+	
+	names(rxnsactivity) <- model@react_id
 	return(rxnsactivity)
 }
