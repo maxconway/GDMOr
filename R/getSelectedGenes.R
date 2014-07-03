d91284dfc5eefed97eb761b743a3333f504c1c1e
maxconway <conway.max1@gmail.com>
2014-07-03 14:01:14 +0100

    Updated and corrected function documwentation
    - made a few small corrections to functions
    - removed demos:
      - cytoscape-setup was amde into an example
      - runscript is obsolete

--- R/getSelectedGenes.R
+++ R/getSelectedGenes.R
@@ -1,6 +1,34 @@
-#' Find the genes associated with the selected nodes in the cytoscape graph
+#' GetSelectedRxns
 #' 
-getSelectedGenes <- function(model, cw){
+#' Retrieve the reaction ids associated with nodes selected in cytoscape
+#' 
+#' @param cw a cytoscape window
+#' @param model a sybil model
+#' 
+#' @return a character vector of reaction ids
+#' 
+#' @family GetSelected*
+#' 
+#' @import RCytoscape sybil
+#' @export
+GetSelectedRxns <- function(cw, model){
 	ids <- getSelectedNodes(cw)
-	genes <- unlist(model@genes[model@react_id %in% ids]) %.% setdiff('') # Using %.% to create and ad-hoc infix operator 
+	rxns <- model@react_id %in% ids %.% setdiff('') # Using %.% to create an ad-hoc infix operator
+}
+
+#' GetSelectedRxns
+#' 
+#' Retrieve the reaction ids associated with nodes selected in cytoscape
+#' 
+#' @param cw a cytoscape window
+#' @param model a sybil model
+#' 
+#' @return a character vector of gene ids
+#' 
+#' @family GetSelected*
+#' 
+#' @import RCytoscape sybil
+#' @export
+getSelectedGenes <- function(cw, model){
+	genes <- unlist(model@genes[GetSelectedRxns(cw, model)]) %.% setdiff('') # Using %.% to create an ad-hoc infix operator 
 }
