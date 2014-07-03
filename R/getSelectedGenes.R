4e17e9940e55e66f4060fcc58244d394bfac081d
maxconway <conway.max1@gmail.com>
2014-07-03 17:47:30 +0100

    renamed from camelCase to underscore seperated
    - also documentation tweaks

--- R/getSelectedGenes.R
+++ R/getSelectedGenes.R
@@ -1,4 +1,4 @@
-#' GetSelectedRxns
+#' Retrieve reactions from Cytoscape
 #' 
 #' Retrieve the reaction ids associated with nodes selected in cytoscape
 #' 
@@ -7,28 +7,28 @@
 #' 
 #' @return a character vector of reaction ids
 #' 
-#' @family GetSelected*
+#' @family GetSelected
 #' 
 #' @import RCytoscape sybil
 #' @export
-GetSelectedRxns <- function(cw, model){
+get_selected_rxns <- function(cw, model){
 	ids <- getSelectedNodes(cw)
 	rxns <- model@react_id %in% ids %.% setdiff('') # Using %.% to create an ad-hoc infix operator
 }
 
-#' GetSelectedRxns
+#' Retrieve genes from Cytoscape
 #' 
-#' Retrieve the reaction ids associated with nodes selected in cytoscape
+#' Retrieve the gene ids associated with nodes selected in cytoscape
 #' 
 #' @param cw a cytoscape window
 #' @param model a sybil model
 #' 
 #' @return a character vector of gene ids
 #' 
-#' @family GetSelected*
+#' @family GetSelected
 #' 
 #' @import RCytoscape sybil
 #' @export
-getSelectedGenes <- function(cw, model){
+get_selected_genes <- function(cw, model){
 	genes <- unlist(model@genes[GetSelectedRxns(cw, model)]) %.% setdiff('') # Using %.% to create an ad-hoc infix operator 
 }
